2201b9401247ab7584cc1d714f40096ef89d97db
saeid yousefi <saeid.rym32@gmail.com>
2020-01-03 19:06:51 +0330

    home page design

---
+++ app/src/main/java/com/pg/personalaccounting/view/deposit_withdraw/DepositWithdrawActivity.kt
@@ -0,0 +1,13 @@
+package com.pg.personalaccounting.view.deposit_withdraw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pg.personalaccounting.R
+
+class DepositWithdrawActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deposit_withdraw)
+    }
+}
--- app/src/main/java/com/pg/personalaccounting/view/home/HomeFragment.kt
+++ app/src/main/java/com/pg/personalaccounting/view/home/HomeFragment.kt
@@ -1,9 +1,14 @@
 package com.pg.personalaccounting.view.home
 
+import android.annotation.SuppressLint
+import android.widget.CalendarView
 import com.pg.personalaccounting.R
 import com.pg.personalaccounting.core.bases.BaseFragment
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class HomeFragment : BaseFragment(R.layout.fragment_home) {
+class HomeFragment : BaseFragment(R.layout.fragment_home), CalendarView.OnDateChangeListener {
 
     companion object {
         private var INSTANCE: BaseFragment? = null
@@ -16,7 +21,18 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
     }
 
     override fun afterLoadView() {
-
+        selectedDateTV.text = getTodayDate()
+        calendarView.setOnDateChangeListener(this)
     }
 
+    @SuppressLint("SetTextI18n")
+    override fun onSelectedDayChange(view: CalendarView, year: Int, month: Int, day: Int) {
+        selectedDateTV.text = "$day/${month + 1}/$year"
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun getTodayDate(): String {
+        val sdf = SimpleDateFormat("dd/M/yyyy")
+        return sdf.format(Date())
+    }
 }
